f9143d28b7055377c284e5fd5a2f2a71a27d83c8
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2025-06-05 12:21:27 +0300

    feat: add bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
 /* @refresh reload */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { render } from 'solid-js/web';
 
 import './index.css';
