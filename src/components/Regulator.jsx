27bc00c2d1511b07010f06bdcf636c577d6ca285
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2025-06-05 14:42:43 +0300

    fix: add validation

--- src/components/Regulator.jsx
+++ src/components/Regulator.jsx
@@ -1,21 +1,33 @@
 import { createSignal } from 'solid-js';
 
+
 export default function Regulator(props){
-    const [property, setProperty] = createSignal(0)
+    const [property, setProperty] = createSignal(props.defaultValue)
     
     return(
-        <div class='regulator'>
-            <span>{props.title}</span>
-            <input type='text' maxLength='3' value={property()} 
-            onInput={(e) => {
-                let value = e.target.value;
-                value = value.replace(/\D/g, "")
-                if (+value > 255) value = "255";
-                if (+value < 0) value = "0"
-                setProperty(value);
-            }}
-            />
-            <input type='range' min='0' max='255' value={property()} onInput={(e) => setProperty(e.target.value)}/>
+        <div class='container'>
+                <div class='row'>
+                    <span class='col-2'>{props.title}</span>
+                    <input class='col-1 numberRange' type='number' value={property()} 
+                        onInput={(e) => {
+                            let value = e.target.value;
+                            value = value.replace(/\D/g, "")
+                            value = value.replace(/^0+(?!$)/, "");   
+                            if (+value < props.minValue)
+                            {
+                                value = props.minValue;
+                            } 
+                            if (+value > props.maxValue) 
+                            {
+                                value = props.maxValue;
+                            }
+                            e.target.value = value;
+                            setProperty(value);
+                        }}
+                    />
+                    <input class='col-8 p-0 range' type='range' min={props.minValue} max={props.maxValue} value={property()} onInput={(e) => setProperty(e.target.value)}/>
+            </div>
+            
         </div>
     )
 }
