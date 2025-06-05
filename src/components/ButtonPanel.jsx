0043eb75128f7826188a4496537c7824b98dd98e
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2025-06-05 16:50:45 +0300

    feat: new button 'close connection'

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -1,8 +1,13 @@
 export default function ButtonPanel(){
     return(
         <div class="button-panel">
-            <button class="btn btn-success btn-save">Save</button>
-            <button class="btn btn-primary btn-ref">Refresh</button>
+            <div class="danger col-6">
+                <button class="btn btn-danger">Close connection</button>
+            </div>
+            <div class="save-ref col-6">
+                <button class="btn btn-success btn-save">Save</button>
+                <button class="btn btn-primary btn-ref">Refresh</button>
+            </div>
         </div>
     )
 }
