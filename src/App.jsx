baf18439fc65229550e40267a704b7dd6cf98cad
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2025-06-05 17:01:36 +0300

    feat: configuration file for min, max, default value

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import ButtonPanel from "./components/ButtonPanel";
 import PIDComponent from "./components/PIDComponent"
+import { settings } from "./config";
 
 function App() {
   return (
     <div>
-      <PIDComponent title={"something 1"} minValue={0} maxValue={100} defaultValue={50}/>
-      <PIDComponent title={"something 2"} minValue={0} maxValue={100} defaultValue={50}/>
-      <PIDComponent title={"something 3"} minValue={0} maxValue={100} defaultValue={50}/>
+      <PIDComponent 
+        title={"something 1"} 
+        minValue={settings.minValue_A} 
+        maxValue={settings.maxValue_A} 
+        defaultValue={settings.defaultValue_A}/>
+      
+      <PIDComponent 
+        title={"something 2"} 
+        minValue={0} 
+        maxValue={100} 
+        defaultValue={1}/>
+      
+      <PIDComponent 
+        title={"something 3"} 
+        minValue={0} 
+        maxValue={100} 
+        defaultValue={1}/>
+      
       <ButtonPanel/>
     </div>
   );
---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const settings = {
+    minValue_A: 10,
+    maxValue_A: 999,
+    defaultValue_A: 255
+  };
